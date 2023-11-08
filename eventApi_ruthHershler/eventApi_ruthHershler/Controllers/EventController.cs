b41911c15d493f04420026d986dcafd41254a15d
ruth7873 <ruth0533137873@gmail.com>
2023-11-08 16:09:20 +0200

    my homework in .net, first commit

---
+++ eventApi_ruthHershler/eventApi_ruthHershler/Controllers/EventController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eventApi_ruthHershler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        private static List<Event> list = new List<Event> { new Event() {Id=0,
+            Start = new DateTime(2023,9,7) }};
+        private static int id = 1;
+        // GET: api/<EventController>
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+            return list;
+        }       
+
+        // POST api/<EventController>
+        [HttpPost]
+        public void Post([FromBody] Event newEvent)
+        {
+            list.Add(new Event() { Id = id++, Title = newEvent.Title, Start = newEvent.Start, End = newEvent.Start });
+        }
+
+        // PUT api/<EventController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Event newEvent)
+        {
+            Event e=list.Find(e=>e.Id==id);
+            Event e1 = new Event(id, newEvent.Title, newEvent.Start, e.End);
+            list.Remove(e);
+            list.Add(e1);
+        }
+
+        // DELETE api/<EventController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+           var l= list.Find(e=>e.Id==id);
+            list.Remove(l);
+        }
+    }
+}
---
+++ eventApi_ruthHershler/eventApi_ruthHershler/Event.cs
@@ -0,0 +1,26 @@
+﻿namespace eventApi_ruthHershler
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public Event()
+        {
+            
+        }
+        public Event(int id,string title)
+        {
+            Id = id;
+            Title = title;
+        }
+        public Event(int id, string title, DateTime start, DateTime end)
+        {
+            Id = id;
+            Title = title;
+            Start = start;
+            End = end;
+        }
+    }
+}
